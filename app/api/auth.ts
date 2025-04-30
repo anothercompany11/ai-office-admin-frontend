1e1ed9163d5a1e22be49b430c20c4c8cf927d74d
amisu1203 <doh_0112@naver.com>
2025-04-30 11:09:34 +0900

    feat: auth 관련 dto 생성

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -14,6 +14,18 @@ export const postLogin = async (data: LoginPostReq) => {
   );
 };
 
+// 로그아웃 요청
+export const postLogout = async () => {
+  try {
+    await apiFetch(`/auth/logout`, {
+      method: "POST",
+    });
+  } catch (err) {
+    console.error(err);
+    throw new Error("로그아웃에 실패했습니다."); // 에러 다시 던지기
+  }
+};
+
 // 토큰 재발급 요청
 export const postRefreshToken = async (csrf_token: string) => {
   return apiFetch(
