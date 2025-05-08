f7745dca249939fea3372e197ec2647f3acd1cbc
amisu1203 <doh_0112@naver.com>
2025-05-08 13:39:52 +0900

    feat: 관리자 계정 생성 성공/실패 토스트 추가

--- app/(GNB-layout)/account/_components/admin-create-modal.tsx
+++ app/(GNB-layout)/account/_components/admin-create-modal.tsx
@@ -1,18 +1,16 @@
 "use client";
 import CustomInputField from "@/app/shared/custom-input-field";
+import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import useCreateAdminAccount from "@/hooks/use-create-account";
+import { accountCreateSchema, AccountCreateSchema } from "@/schema/account";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { XIcon } from "lucide-react";
 import { useEffect } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-
-import { Button } from "@/components/ui/button";
-import useCreateAdminAccount from "@/hooks/use-create-account";
-import { accountCreateSchema, AccountCreateSchema } from "@/schema/account";
-import { DialogTitle } from "@radix-ui/react-dialog";
 
 interface Props {
   open: boolean;
@@ -43,7 +41,6 @@ export default function AdminCreateModal({ open, onOpenChange, page, size }: Pro
   const useDefault = watch("useDefaultPassword");
 
   const onSubmit: SubmitHandler<AccountCreateSchema> = async (vals) => {
-    // 토글 켰으면 서버로 '00000000' 넘김
     const payload = {
       ...vals,
       password: vals.useDefaultPassword ? "00000000" : vals.password,
@@ -61,9 +58,8 @@ export default function AdminCreateModal({ open, onOpenChange, page, size }: Pro
         onOpenChange(o);
       }}
     >
-      <DialogContent className="w-full max-w-[400px]">
-        <DialogTitle />
-        <div className="flex justify-between items-center mb-8 border">
+      <DialogContent className="w-full max-w-[400px] gap-10">
+        <div className="flex justify-between items-center">
           <p className="text-title-l">관리자 생성하기</p>
           <button onClick={() => onOpenChange(false)}>
             <XIcon className="size-6 text-component" />
@@ -71,65 +67,72 @@ export default function AdminCreateModal({ open, onOpenChange, page, size }: Pro
         </div>
 
         <Form {...form}>
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-            <CustomInputField
-              form={form}
-              name="name"
-              placeholder="이름을 입력해 주세요"
-              label="이름"
-              isValid={!formState.errors.name}
-              validText={formState.errors.name?.message}
-            />
+          <form onSubmit={handleSubmit(onSubmit)} className="">
+            <div className="flex flex-col gap-6">
+              <CustomInputField
+                form={form}
+                name="name"
+                placeholder="이름을 입력해 주세요"
+                label="이름"
+                isValid={!formState.errors.name}
+              />
 
-            <CustomInputField
-              form={form}
-              name="login_id"
-              placeholder="아이디를 입력해 주세요"
-              label="아이디"
-              isValid={!formState.errors.login_id}
-              validText={formState.errors.login_id?.message}
-            />
+              <CustomInputField
+                form={form}
+                name="login_id"
+                placeholder="아이디를 입력해 주세요"
+                label="아이디"
+                isValid={!formState.errors.login_id}
+              />
 
-            {/* 초기 비밀번호 설정 토글 */}
-            <FormField
-              control={control}
-              name="useDefaultPassword"
-              render={({ field }) => (
-                <FormItem className="flex items-center gap-2">
-                  <FormControl>
-                    <Checkbox
-                      checked={field.value}
-                      onCheckedChange={(checked) => {
-                        field.onChange(checked);
-                        // 토글 켜면 비밀번호 자동 세팅, 끄면 빈 값
-                        setValue("password", checked ? "00000000" : "");
-                      }}
-                    />
-                  </FormControl>
-                  <FormLabel>초기 비밀번호 설정</FormLabel>
-                </FormItem>
-              )}
-            />
-
-            {/* 비밀번호 입력 */}
-            <FormField
-              control={control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>비밀번호</FormLabel>
-                  <FormControl>
-                    <Input {...field} type="password" placeholder="비밀번호를 입력해주세요." disabled={useDefault} />
-                  </FormControl>
-                  {formState.errors.password && (
-                    <p className="mt-1 text-xs text-red-600">{formState.errors.password.message}</p>
+              {/* 비밀번호 입력 */}
+              <div className="flex flex-col gap-3">
+                <FormField
+                  control={control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>비밀번호</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="비밀번호를 입력해주세요."
+                          disabled={useDefault}
+                        />
+                      </FormControl>
+                    </FormItem>
                   )}
-                </FormItem>
-              )}
-            />
+                />
+                {/* 초기 비밀번호 설정 토글 */}
+                <FormField
+                  control={control}
+                  name="useDefaultPassword"
+                  render={({ field }) => (
+                    <FormItem className="flex items-center gap-2">
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          onCheckedChange={(checked) => {
+                            field.onChange(checked);
+                            // 토글 켜면 비밀번호 자동 세팅, 끄면 빈 값
+                            setValue(
+                                "password",
+                                checked ? "00000000" : "",
+                                { shouldValidate: true, shouldDirty: true }
+                              );
+                          }}
+                        />
+                      </FormControl>
+                      <FormLabel>초기 비밀번호 설정</FormLabel>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
 
-            <div className="flex justify-end gap-2">
+            <div className="flex justify-end gap-2 mt-10">
               <Button
+                size="lg"
                 variant="outline"
                 onClick={() => {
                   reset();
