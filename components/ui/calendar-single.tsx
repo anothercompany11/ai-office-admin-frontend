48895a31d2021c57dbcd20e6bca303306bae5c88
amisu1203 <doh_0112@naver.com>
2025-05-09 16:39:26 +0900

    chore: ui 수정

--- components/ui/calendar-single.tsx
+++ components/ui/calendar-single.tsx
@@ -14,6 +14,7 @@ type CalendarSingleProps = {
 
 const CalendarSingle = ({ date, setDate }: CalendarSingleProps) => {
   const today = new Date();
+  const isAllSelected = date.from && date.to;
 
   const onSelectRange = (newRange: DateRange | undefined) => {
     if (newRange) {
@@ -23,7 +24,7 @@ const CalendarSingle = ({ date, setDate }: CalendarSingleProps) => {
 
   // 캘린더 표시 문구
   let displayText = "날짜 선택";
-  if (date.from && date.to) {
+  if (isAllSelected) {
     displayText = `${formattedDate(date.from)} ~ ${formattedDate(date.to)}`;
   } else if (date.from) {
     displayText = formattedDate(date.from);
@@ -37,7 +38,7 @@ const CalendarSingle = ({ date, setDate }: CalendarSingleProps) => {
         <button
           className={cn("w-[250px] relative py-3 px-4 rounded-sm border flex items-center justify-between text-[15px]")}
         >
-          <span className="text-body-m text-label-alternative">{displayText}</span>
+          <span className={`text-body-m ${isAllSelected ? "" : "text-label-alternative"}`}>{displayText}</span>
           <CalendarIcon className="absolute inset-y-0 right-0 my-auto ml-auto mr-3 size-[19px] text-black" />
         </button>
       </PopoverTrigger>
